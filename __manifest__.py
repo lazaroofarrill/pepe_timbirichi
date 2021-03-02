8afeb9d896e50a1caa8b44cdb5dc6fc81d1ae553
Alberto Roman Ramos <bdalbert2909@gmail.com>
2021-03-02 15:58:45 -0500

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'pepe_timbirichi',
+    'version': '13.0.0.1',
+    'summary': 'E-commerce platform',
+    'description': """An e-commerce platform for selling anything""",
+    'category': 'Uncategorized',
+    'author': 'Alberto',
+    'website': '',
+    'license': '',
+    'depends': [],
+    'data': [],
+    'installable': True
+}
